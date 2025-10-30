9aab248deef656b9c684cee393c91c8acb217abc
Ignasius <96295999+ignasius-j-s@users.noreply.github.com>
2025-10-30 20:08:00 +0700

    relax trait bounds

--- src/util/io.rs
+++ src/util/io.rs
@@ -1,38 +1,38 @@
 use std::io::{Read, Result, Write};
 
 pub trait Endian: Sized {
-    fn read_le<R: Read>(reader: &mut R) -> Result<Self>;
-    fn read_be<R: Read>(reader: &mut R) -> Result<Self>;
-    fn write_le<W: Write>(self, writer: &mut W) -> Result<()>;
-    fn write_be<W: Write>(self, writer: &mut W) -> Result<()>;
+    fn read_le<R: Read + ?Sized>(reader: &mut R) -> Result<Self>;
+    fn read_be<R: Read + ?Sized>(reader: &mut R) -> Result<Self>;
+    fn write_le<W: Write + ?Sized>(self, writer: &mut W) -> Result<()>;
+    fn write_be<W: Write + ?Sized>(self, writer: &mut W) -> Result<()>;
 }
 
 macro_rules! impl_endian {
     ($($type:ty),*) => ($(
         impl Endian for $type {
             #[inline]
-            fn read_le<R: Read>(reader: &mut R) -> Result<Self> {
+            fn read_le<R: Read + ?Sized>(reader: &mut R) -> Result<Self> {
                 let mut buf = [0; size_of::<$type>()];
                 reader.read_exact(&mut buf)?;
                 Ok(<$type>::from_le_bytes(buf))
             }
 
             #[inline]
-            fn read_be<R: Read>(reader: &mut R) -> Result<Self> {
+            fn read_be<R: Read + ?Sized>(reader: &mut R) -> Result<Self> {
                 let mut buf = [0; size_of::<$type>()];
                 reader.read_exact(&mut buf)?;
                 Ok(<$type>::from_be_bytes(buf))
             }
 
             #[inline]
-            fn write_le<W: Write>(self, writer: &mut W) -> Result<()> {
+            fn write_le<W: Write + ?Sized>(self, writer: &mut W) -> Result<()> {
                 let buf = <$type>::to_le_bytes(self);
                 writer.write_all(&buf)?;
                 Ok(())
             }
 
             #[inline]
-            fn write_be<W: Write>(self, writer: &mut W) -> Result<()> {
+            fn write_be<W: Write + ?Sized>(self, writer: &mut W) -> Result<()> {
                 let buf = <$type>::to_be_bytes(self);
                 writer.write_all(&buf)?;
                 Ok(())
@@ -44,7 +44,7 @@ macro_rules! impl_endian {
 impl_endian!(i8, i16, i32, i64);
 impl_endian!(u8, u16, u32, u64);
 
-pub trait ReadExt: Read + Sized {
+pub trait ReadExt: Read {
     #[inline]
     fn read_bytes<const N: usize>(&mut self) -> Result<[u8; N]> {
         let mut buf = [0_u8; N];
@@ -63,7 +63,7 @@ pub trait ReadExt: Read + Sized {
     }
 }
 
-pub trait WriteExt: Write + Sized {
+pub trait WriteExt: Write {
     #[inline]
     fn write_bytes<B: AsRef<[u8]>>(&mut self, bytes: B) -> Result<()> {
         self.write_all(bytes.as_ref())
